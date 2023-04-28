14ab50331a238987ae252458e04b77c489cf144b
Djura Neskovic <neskovic991@gmail.com>
2023-04-28 22:57:30 +0200

    spacing

--- src/components/styles/Header.styled.js
+++ src/components/styles/Header.styled.js
@@ -5,6 +5,7 @@ import styled from "styled-components";
 export const StyledHeader = styled.header`
   background-color: #ebfbff;
   padding: 40px;
+  
   /* Like in SASS we can nest styles */
   h1 {
     color: red;
