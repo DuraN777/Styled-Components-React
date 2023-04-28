43444fc3c79aa427324d0e8fbe0988a1085dc926
Djura Neskovic <neskovic991@gmail.com>
2023-04-28 23:15:55 +0200

    removed a prop

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import { StyledHeader } from "./styles/Header.styled";
 
 const Header = () => {
   return (
-    <StyledHeader bg="red">  {/* Replace <header> tag with styled-component  StyledHeader */}
+    <StyledHeader>  {/* Replace <header> tag with styled-component  StyledHeader */}
       <h1>Hubble</h1>
     </StyledHeader>
   );
