746098422d6ce09026f92a084dc408db498c1f50
Djura Neskovic <neskovic991@gmail.com>
2023-04-28 22:54:13 +0200

    header with styled-comp

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import { StyledHeader } from "./styles/Header.styled";
+
+const Header = () => {
+  return (
+    <StyledHeader>  {/* Replace <header> tag with styled-component  StyledHeader */}
+      <h1>Hubble</h1>
+    </StyledHeader>
+  );
+}
+ 
+export default Header;
