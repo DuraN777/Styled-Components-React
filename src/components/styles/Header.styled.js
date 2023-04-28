365aa28586a8c783bf10e745d07ddc1d21f562a7
Djura Neskovic <neskovic991@gmail.com>
2023-04-28 22:53:50 +0200

    styled-comp for header

---
+++ src/components/styles/Header.styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+// this styled header I bring to Header component
+
+// var StyledName = styled.tag 
+export const StyledHeader = styled.header`
+  background-color: #ebfbff;
+  padding: 40px;
+  {/* Like in SASS we can nest styles*/}
+  h1 {
+    color: red;
+  }
+`
