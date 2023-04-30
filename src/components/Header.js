bf455e47c551b7bf9755b0df1007b75f4c50da19
Djura Neskovic <neskovic991@gmail.com>
2023-04-30 11:53:31 +0200

    Create Nav,Logo and Button for Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
-import { StyledHeader } from "./styles/Header.styled";
+import { StyledHeader, Nav, Logo } from "./styles/Header.styled";
 import { Container } from "./styles/Container.styled";
+import { Button } from "./styles/Button.styled";
 
 const Header = () => {
   return (
     <StyledHeader>  {/* Replace <header> tag with styled-component  StyledHeader */}
       <Container>
-        <h1>Hubble</h1>
+        <Nav>
+          <Logo src="./images/logo.svg" alt="Huddle logo" />
+          <Button>Try It Free</Button>
+        </Nav>
       </Container>
     </StyledHeader>
   );
--- src/components/styles/Header.styled.js
+++ src/components/styles/Header.styled.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 // this styled header I bring to Header component
+// Since Nav and Logo tags are used only in header, I define them in Header.styled component
 
 // var StyledName = styled.tag 
 export const StyledHeader = styled.header`
@@ -10,4 +11,12 @@ export const StyledHeader = styled.header`
   /* h1 {
     color: #fff;
   } */
+`
+
+export const Nav = styled.nav`
+  padding: 20px;
+`
+
+export const Logo = styled.img`
+  max-width: 30px;
 `
